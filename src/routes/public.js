404106a07962b9d698151d94be51a904da286bfb
Your Name <you@example.com>
2024-09-19 14:47:32 +0100

    message:cadastro

--- src/routes/public.js
+++ src/routes/public.js
@@ -1,3 +1,34 @@
 import express from 'express';
+import { Prisma } from '@prisma/client';
+import bycrpt, { compare } from 'bcrypt'
 const app=express()
-rou
+const router=express.Router()
+const prisma=new Prisma()
+
+router.get(3000,(req,res)=>{
+    res.send('test')
+})
+
+
+//cadastro
+router.post('/cadastro',async(req,res)=>{
+    try{
+    const {nome,email,senha}=req.body
+    const salt=await bycrpt.genSalt(10)
+    const hashpassword=await bycrpt.hash(senha,salt)
+    const user=await prisma.user.create({
+        data:{
+            nome,
+            email,
+            senha:hashpassword,
+        }
+    })
+    res.status(201).send({user})
+    }catch(err){
+    res.status(400).send({message:message.err})
+    }
+})
+
+//login
+
+export  default router;
