662fa0bac7d1c2342b3b9fb6dffc3a59cdbec098
Your Name <you@example.com>
2024-09-20 20:05:29 +0100

    message:finalizando api

--- src/routes/private.js
+++ src/routes/private.js
@@ -4,6 +4,7 @@ import  express  from "express";
 const router=express.Router(); ;
 
 //listar os usuarios
+
 router.get("/users-list",async (req,res)=>{
     try{
     const users=await prisma.user.findMany();
